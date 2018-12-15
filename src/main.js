e228283c1b3b33fc36b7ee286852bddf6bbc2d97
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-12-15 13:00:00 +0100

    More realistic node setup

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/secret', (req, res) => res.send(`The super secret value is ${process.env.SECRET}`))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/main.spec.js
@@ -0,0 +1,5 @@
+describe('sample', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3)
+  })
+})
